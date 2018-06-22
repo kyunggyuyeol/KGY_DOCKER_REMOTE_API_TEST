b10460df5983a4224799a7ef710c66feef09310c
경규열 <p82468@paran.com>
2018-06-22 11:58:30 +0900

    이미지 리스트 가져오기

--- dockerdotnetTest/Form1.cs
+++ dockerdotnetTest/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Docker;
@@ -40,24 +41,55 @@ namespace dockerdotnetTest
             //실행 중인 컨테이너 리스트 출력
             IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(new ContainersListParameters() { Limit = 10 });
 
+            //docker images list 
+            IList<ImagesListResponse> images = await client.Images.ListImagesAsync(new ImagesListParameters(), CancellationToken.None);
+            var aa = images[0].Labels.TryGetValue("org.label-schema.schema-version", out string outstring);
 
+
+            //컨테이터 아이디 불러옴
+            string stopcontiner = containers[0].ID;
+            var stopped = await client.Containers.StopContainerAsync(stopcontiner, new ContainerStopParameters()
+            {
+                WaitBeforeKillSeconds = 10
+            },
+            CancellationToken.None);
 
 
             //docker image 를 docker hub에서 다운로드함.
             //임시로 내 테스트 업로드용 업데이트
-            var report = new Progress<JSONMessage>(msg =>
-            {
-                Console.WriteLine($"{msg.Status}|{msg.ProgressMessage}|{msg.ErrorMessage}");
-            });
+            //string image = "p82468/kgy";
+            //try
+            //{
+            //    var report = new Progress<JSONMessage>(msg =>
+            //    {
+            //        Console.WriteLine($"{msg.Status}|{msg.ProgressMessage}|{msg.ErrorMessage}");
+            //    });
 
-            // pull image again
-            await client.Images.CreateImageAsync(new ImagesCreateParameters
-            {
-                FromImage = "p82468/kgy"
-            },
-            new AuthConfig(),
-            report
-            );
+            //    // pull image again
+            //    await client.Images.CreateImageAsync(new ImagesCreateParameters
+            //    {
+            //        FromImage = image
+            //    },
+            //    new AuthConfig(),
+            //    report
+            //    );
+
+            //    Console.WriteLine($"{Environment.NewLine}Successfully pulled image {image} to {client.Configuration.EndpointBaseUri}");
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine($"Exception thrown attempting to pull image {image} to {client.Configuration.EndpointBaseUri}");
+            //    Console.WriteLine($"{ex}");
+            //}
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DockerClient client = new DockerClientConfiguration(new Uri("http://192.168.0.25:4243")).CreateClient();
+
+
 
 
         }
